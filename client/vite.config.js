2bea7b49959e7836e77d0444649f81b92b9a7895
Princeratheee <rathi.prince2@gmail.com>
2025-08-05 10:18:18 +0530

    Fixing issues of V0

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5880', // Change this if your backend runs on a different port
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
